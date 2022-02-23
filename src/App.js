1ddcdb161f64005f3e182baded92a268ce5b72c1
coderighter2 <24811616+coderighter2@users.noreply.github.com>
2022-02-23 10:32:35 +0200

    front end for server side backend Eng Test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Calculation from './Calculation';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>Pharaoh of Egypt is seeking job applicants.</h2>
+      <Calculation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Calculation.js
@@ -0,0 +1,142 @@
+
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '300px',
+    },
+    '& .MuiButtonBase-root': {
+      margin: theme.spacing(2),
+    },
+  },
+}));
+
+const DefaultValues = {
+  totalTime: 120,
+  loadTime: 1,
+  unloadTime: 2,
+  boatsCount: 20,
+  locationTime: 10
+}
+
+const Calculation = () => {
+  const classes = useStyles();
+
+  const [totalTime, setTotalTime] = useState(DefaultValues.totalTime);
+  const [loadTime, setloadTime] = useState(DefaultValues.loadTime);
+  const [unloadTime, setUnloadTime] = useState(DefaultValues.unloadTime);
+  const [boatsCount, setBoatsCount] = useState(DefaultValues.boatsCount);
+  const [locationTime, setLocationTime] = useState(DefaultValues.locationTime);
+
+  const [amount, setAmount] = useState(null);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const queryString = new URLSearchParams({
+      totalTime, loadTime, unloadTime, boatsCount, locationTime
+    }).toString();
+    fetch(`http://localhost:5000/stones-amount?${queryString}`)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setAmount(data.amount)
+      })
+  };
+  const setAsDefault = e => {
+    e.preventDefault();
+    setTotalTime(DefaultValues.totalTime)
+    setloadTime(DefaultValues.loadTime)
+    setUnloadTime(DefaultValues.unloadTime)
+    setBoatsCount(DefaultValues.boatsCount)
+    setLocationTime(DefaultValues.locationTime)
+  }
+  
+  return (
+    <>
+      <form className={classes.root} onSubmit={handleSubmit}>
+        <TextField
+          label="Total Time"
+          variant="filled"
+          value={totalTime}
+          type="number"
+          min={1}
+          max={100}
+          required
+          onChange={e => setTotalTime(e.target.value)}
+        />
+
+        <TextField
+          label="A block loading time"
+          variant="filled"
+          value={loadTime}
+          type="number"
+          min={1}
+          max={100}
+          required
+          onChange={e => setloadTime(e.target.value)}
+        />
+
+        <TextField
+          label="A block Unloading time"
+          variant="filled"
+          value={unloadTime}
+          type="number"
+          min={1}
+          max={100}
+          required
+          onChange={e => setUnloadTime(e.target.value)}
+        />
+
+        <TextField
+          label="Total number of Boats"
+          variant="filled"
+          value={boatsCount}
+          type="number"
+          min={1}
+          max={100}
+          required
+          onChange={e => setBoatsCount(e.target.value)}
+        />
+
+        <TextField
+          label="Time between locations"
+          variant="filled"
+          value={locationTime}
+          type="number"
+          min={1}
+          max={100}
+          required
+          onChange={e => setLocationTime(e.target.value)}
+        />
+
+        <div>
+          <Button variant="contained" color="secondary" onClick={setAsDefault}>
+            Set As default
+          </Button>
+        </div>
+
+        <div>
+          <Button type="submit" variant="contained" color="primary">
+            Get Maxium Amounts of stones
+          </Button>
+        </div>
+      </form>
+      {amount === 0 && <p>No Delivered Stones</p>}
+      {amount > 0 && <h4 color="primary">You can deliver maxium {amount} stones now.</h4>}
+    </>
+  );
+};
+
+export default Calculation;
